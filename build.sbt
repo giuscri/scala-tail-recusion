3e004f998e1534f11fa037edb3f9a321740d633c
Giuseppe <giuscri@gmail.com>
2021-02-25 15:14:34 +0100

    reproduce bug

--- build.sbt
+++ build.sbt
@@ -1,2 +1,3 @@
 libraryDependencies += "org.typelevel" %% "cats-effect" % "2.3.1"
-
+libraryDependencies += "ch.qos.logback" % "logback-classic" % "1.2.3"
+libraryDependencies += "com.typesafe.scala-logging" %% "scala-logging" % "3.9.2"
--- src/main/scala/Boot.scala
+++ src/main/scala/Boot.scala
@@ -1,11 +1,19 @@
 package main
 
 import cats.effect._
+import com.typesafe.scalalogging.LazyLogging
 
-object Main extends IOApp {
+object Main extends IOApp with LazyLogging {
+
+	def f(): Int = {
+		val x = List.fill(1000000)(List(1)).flatten // Generate a 1M numbers list
+		1 + f()
+	}
 
 	def run(args: List[String]): IO[ExitCode] = {
-		IO(println("Hello!")).as(ExitCode.Success)
+		f()
+
+		IO.pure(ExitCode.Error)
 	}
 
 }
